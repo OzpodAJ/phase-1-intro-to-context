36b9ea432b34df9c5dee882e35420a0fe2bee147
Andrew Osborn <osbornandrewj@gmail.com>
2021-12-09 11:35:01 -0800

    commit tree 84ead06e5bf1b631df8a924e156b9ca5783f1068

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function createEmployeeRecord (array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+function createEmployeeRecords(employeeArray){
+    return employeeArray.map(p1 => createEmployeeRecord(p1))
+}
+
+const createTimeInEvent = function(){}
